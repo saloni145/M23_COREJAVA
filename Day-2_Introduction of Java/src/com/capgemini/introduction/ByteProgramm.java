d5fc41da4a9f43b48791688fee72d0ba67cf8f8e
HP <HP@LAPTOP-Q8FSUAT2>
2022-08-02 15:21:26 +0530

    First Commit

---
+++ Day-2_Introduction of Java/src/com/capgemini/introduction/ByteProgramm.java
@@ -0,0 +1,11 @@
+package com.capgemini.introduction;
+
+public class ByteProgramm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         byte a = 127;
+         System.out.println(a);
+	}
+
+}
