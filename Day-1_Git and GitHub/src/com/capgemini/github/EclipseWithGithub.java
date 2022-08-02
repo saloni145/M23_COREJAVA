03ae475e89ef51b59b49dd38eabe3c878dfde2e5
HP <HP@LAPTOP-Q8FSUAT2>
2022-08-02 12:10:37 +0530

    First Commit

---
+++ Day-1_Git and GitHub/src/com/capgemini/github/EclipseWithGithub.java
@@ -0,0 +1,10 @@
+package com.capgemini.github;
+//demo for the connect Eclipse with GitHub
+public class EclipseWithGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World");
+	}
+
+}
